b7130ebdb152c7172c508956be3ee695124b31c4
ZokirS <zokir5857@gmail.com>
2023-07-19 12:02:38 +0500

    add new project for theme Dependency Injection tutorial

---
+++ 14-Dependency_Injection/Platform/Population.cs
@@ -0,0 +1,30 @@
+﻿namespace _13_URL_Routing.Platform
+{
+    public class Population
+    {
+        public static async Task Endpoint(HttpContext context)
+        {
+            string? city = context.Request.RouteValues["city"] as string ?? "london";
+            int? pop = null;
+            switch((city ?? "").ToLower())
+            {
+                case "london":
+                    pop = 8_132_000;
+                    break;
+                case "paris":
+                    pop = 4_000_000;
+                    break;
+                case "monaco":
+                    pop = 39_000;
+                    break;
+            }
+            if (pop.HasValue)
+                await context.Response
+                    .WriteAsync($"City: {city}, population: {pop}");
+            else
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+            
+        }
+        
+    }
+}
