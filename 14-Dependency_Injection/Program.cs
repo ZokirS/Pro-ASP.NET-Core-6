4d90d2710bb130ed8dcfcf8ef39abf95b2f17bf1
Zokir Sobirjonov <33254311+ZokirS@users.noreply.github.com>
2023-08-01 11:43:46 +0500

    add logging options for different layer of app

--- 14-Dependency_Injection/Program.cs
+++ 14-Dependency_Injection/Program.cs
@@ -1,6 +1,8 @@
+using _13_URL_Routing.Platform;
 using _14_Dependency_Injection.Platform;
 using _14_Dependency_Injection.Platform.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.HttpLogging;
 
 //var builder = WebApplication.CreateBuilder(args);
 
@@ -99,7 +101,18 @@ using Microsoft.Extensions.DependencyInjection;
 //Chapter 15 - Using the Platform Features, Part 1
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpLogging(opts =>
+{
+    opts.LoggingFields = HttpLoggingFields.RequestMethod
+    | HttpLoggingFields.RequestPath | HttpLoggingFields.ResponseStatusCode;
+});
 var app = builder.Build();
+app.UseHttpLogging();
+var logger = app.Services
+.GetRequiredService<ILoggerFactory>().CreateLogger("Pipeline");
+//logger.LogDebug("Pipeline configuration starting");
+app.MapGet("population/{city?}", Population.Endpoint);
+//logger.LogDebug("Pipeline configuration complete");
 app.MapGet("/", async context => {
     await context.Response.WriteAsync("Hello World!");
 });
